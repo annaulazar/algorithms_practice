57a94719b9869bfe6f16a3b3447bceae39b85075
Anna Lazarenko <oleglazarenko@yandex.ru>
2023-04-20 23:36:35 +0700

    3.0 divA tasks 11-15

--- Con3.0_divA/lecture_1/task_13.py
+++ Con3.0_divA/lecture_1/task_13.py
@@ -12,4 +12,38 @@ s = list(input())
 operators = {'|': 0, '^': 0, '&': 1, '!': 2}
 postf = []
 stack = []
-f_open = 0
+for i in range(len(s)):
+    if s[i].isdigit():
+        postf.append(s[i])
+    elif s[i] in operators:
+        while stack and operators.get(stack[-1], -1) >= operators[s[i]]:
+            postf.append(stack.pop())
+        stack.append(s[i])
+    elif s[i] == '(':
+        stack.append('(')
+    elif s[i] == ')':
+        while stack[-1] != '(':
+            postf.append(stack.pop())
+        stack.pop()
+
+for _ in range(len(stack)):
+    postf.append(stack.pop())
+
+for sym in postf:
+    if sym.isdigit():
+        stack.append(int(sym))
+    elif sym != '!':
+        arg1 = stack.pop()
+        arg2 = stack.pop()
+        if sym == '|':
+            res = arg2 | arg1
+        elif sym == '&':
+            res = arg2 & arg1
+        elif sym == '^':
+            res = arg2 ^ arg1
+        stack.append(res)
+    elif sym == '!':
+        arg = stack.pop()
+        res = 1 - arg
+        stack.append(res)
+print(stack[-1])
